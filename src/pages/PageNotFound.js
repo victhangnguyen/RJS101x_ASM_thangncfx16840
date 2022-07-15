204b8967de5f743846a56de854fa263a1ed1117a
victhangnguyen <victhangnguyen@outlook.com>
2022-07-16 00:47:06 +0700

    updating PageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 export class PageNotFound extends Component {
   render() {
-    return <div>Page Not Found</div>;
+    return (
+      <div className="page-not-found d-flex justify-content-center align-items-center">
+        <div className="content">
+          <h3 style={{ fontSize: '6rem', display: 'inline-block' }}>Oops!</h3>
+          <p style={{ fontSize: '5rem', display: 'inline-block' }}>404</p>
+          <p style={{ fontSize: '2rem' }}>Page Not Found</p>
+        </div>
+      </div>
+    );
   }
 }
 
